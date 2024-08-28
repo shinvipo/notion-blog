e17b1d202f8020eccdc31762bfd5dbc95aa4a5af
Trần Văn Hải <54126943+shinvipo@users.noreply.github.com>
2024-08-28 15:51:18 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "_HaiTV_",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "EmailTest@gmail.com",
+    linkedin: "Tesst",
+    github: "Tesst",
     instagram: "",
   },
   projects: [
